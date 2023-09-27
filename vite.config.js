412ef42370adbd41d9ac4809075131d4afa08bfc
MarioVicente <mariovicente283@gmial.com>
2023-09-27 11:49:59 -0500

    fix conflic

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/sass/tabla.scss'
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
+            refresh: true,
         }),
     ],
 });
