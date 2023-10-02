c39ad7307fdfe9d4bfa66a69721b4317ec1d5ab5
MarioVicente <mariovicente283@gmial.com>
2023-10-02 12:32:01 -0500

    indexado

--- resources/views/client/form.blade.php
+++ resources/views/client/form.blade.php
@@ -1,23 +1,19 @@
 <link href="{{ asset('css/form.css') }}" rel="stylesheet">
 
-<div >
-   <div >
-       <div >
-           <form>
-               <div>
-                   {{ Form::label('name', 'Name', ) }}
-                   {{ Form::text('name', $client->name, [ ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
-                   {!! $errors->first('name', ) !!}
-               </div>
-               <div >
-                   {{ Form::label('email', 'Email', ) }}
-                   {{ Form::text('email', $client->email, [($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => 'Email']) }}
-                   {!! $errors->first('email') !!}
-               </div>
-               <div >
-                   <button type="submit">{{ __('Submit') }}</button>
-               </div>
-           </form>
-       </div>
-   </div>
+<div>
+    <form>
+        <div>
+            {{ Form::label('name', 'Name', ) }}
+            {{ Form::text('name', $client->name, [ ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+            {!! $errors->first('name', ) !!}
+        </div>
+        <div >
+            {{ Form::label('email', 'Email', ) }}
+            {{ Form::text('email', $client->email, [($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => 'Email']) }}
+            {!! $errors->first('email') !!}
+        </div>
+        <div >
+            <button type="submit">{{ __('Submit') }}</button>
+        </div>
+    </form>
 </div>
--- resources/views/client/show.blade.php
+++ resources/views/client/show.blade.php
@@ -1,52 +1,38 @@
 <x-app-layout>
-   <x-slot name="header">
-       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           {{ __('Dashboard') }}
-       </h2>
-   </x-slot>
-   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-   
-   <div class="py-12">
-       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-           <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg text-center">
-            <div class="float-right">
-               <h3>
-                Client  {{ $client->name ?? "{{ __('Show') Cliente" }}
-              </h3>
-              <section class="content container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    
 
-                    
-                        
-                        <div class="form-group">
-                            <strong>Name:</strong>
-                            {{ $client->name }}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg text-center">
+                <div class="float-right">
+                    <h3>  Client  {{ $client->name ?? "{{ __('Show') Cliente" }}   </h3>
+                    <section class="content container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div>
+                                        <strong>Name:</strong>
+                                        {{ $client->name }}
+                                    </div>
+                                    <div class="form-group">
+                                        <strong>Email:</strong>
+                                        {{ $client->email }}
+                                    </div>
+                                    <div class="float-right">
+                                        <a class="btn btn-primary" href="{{ route('client.index') }}"> {{ __('Back') }}</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <strong>Email:</strong>
-                            {{ $client->email }}
-                        </div>
-                        
-                        
-                        <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('client.index') }}"> {{ __('Back') }}</a>
-                        </div>
-                    </div>
-
-                    </div>
-                </div>
-            </div>
+                    </section>
+                </div> 
+            </div> 
         </div>
-    </section>
-
-
-
-
-           </div> 
-       </div>
-   </div>
+    </div>
 </x-app-layout>
