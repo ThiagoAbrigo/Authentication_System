fe4cf11e2c5d4d5c59b6f315f1222ab4d3cc4acf
ThiagoAbrigo <santiabrigo497@gmail.com>
2023-10-05 11:02:37 -0500

    style modify

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -6,59 +6,60 @@
     </x-slot>
     <div class="container">
         <div class="table">
-                <button class="button-create">
-                    <a  href="{{ route('client.create') }}" >
+            <button class="button-create">
+                <a href="{{ route('client.create') }}">
                     {{ __('Create New') }}</a>
-                </button>
-                <table>
-                    <thead>
-                        <tr>
-                            <th class="head">No</th>
-                            <th class="head">Name</th>
-                            <th class="head">Email</th>
-                            <th class="head">Show Orders</th>
-                            <th class="head">Add Order</th>
+            </button>
+            <table>
+                <thead class="custom-title">
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Show Orders</th>
+                        <th>Add Order</th>
 
-                            <th class="head">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
                     @foreach ($client as $cli)
                         <tr>
-                            <td class="body"> {{++$i}}</td>
-                            <td class="bodyname">{{$cli->name}}</td>
-                            <td class="bodyname">{{$cli->email}}</td>
-                            <td class="body"> 
-                                <a href="{{ route('showorders.index',['client' => $cli->name]) }}"   > <i class="fa-brands fa-shopify "></i>   </a> 
-                            </td>
-                            <td class="body"> 
-                                <a href="{{route('order.index')}}" >
-                                    <i class="fa-solid fa-cart-plus"></i>    
-                                </a> 
+                            <td class="body"> {{ ++$i }}</td>
+                            <td class="bodyname">{{ $cli->name }}</td>
+                            <td class="bodyname">{{ $cli->email }}</td>
+                            <td class="body">
+                                <a href="{{ route('showorders.index', ['client' => $cli->name]) }}"> <i
+                                        class="fa-brands fa-shopify "></i> </a>
                             </td>
                             <td class="body">
-                                <form action="{{ route('client.destroy',$cli->id) }}" method="POST">
-                                    <a href="{{ route('client.show',$cli->id) }}">
-                                        <i class="fa fa-fw fa-eye" show> </i> 
-                                    
+                                <a href="{{ route('order.index') }}">
+                                    <i class="fa-solid fa-cart-plus"></i>
+                                </a>
+                            </td>
+                            <td class="body">
+                                <form action="{{ route('client.destroy', $cli->id) }}" method="POST">
+                                    <a href="{{ route('client.show', $cli->id) }}">
+                                        <i class="fa fa-fw fa-eye" show> </i>
+
                                     </a>
-                                    <a  href="{{ route('client.edit',$cli->id) }}">
-                                        <i class="fa fa-fw fa-edit edit" ></i> 
-                                        
+                                    <a href="{{ route('client.edit', $cli->id) }}">
+                                        <i class="fa fa-fw fa-edit edit"></i>
+
                                     </a>
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" >
-                                        <i class="fa fa-fw fa-trash"></i> 
+                                    <button type="submit">
+                                        <i class="fa fa-fw fa-trash"></i>
                                     </button>
                                 </form>
                             </td>
                         </tr>
                     @endforeach
-                    </tbody>
-                  <!-- Contenido de la tabla -->
-                </table>
-            </div>
+                </tbody>
+                <!-- Contenido de la tabla -->
+            </table>
         </div>
-    
+    </div>
+
 </x-app-layout>
